6266665cc7c01cb0e3b220da2d7609deff6d53cd
lohanlhn <lohan.massote@gmail.com>
2020-06-03 08:37:29 -0300

    Entities criadas

---
+++ Entities/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Class1
+    {
+        public int id { get; set; }
+        public string chassi { get; set; }
+        public string placa { get; set; }
+        public int renavam { get; set; }
+        public Modelo modelo { get; set; }
+    }
+}
---
+++ Entities/Modelo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Modelo
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public Marca marca { get; set; }
+    }
+}
