a61660213b15ea2b33ff7551cf58334b89174f93
lohan <lohan.massote.dev@gmail.com>
2020-06-06 11:42:57 -0300

    Alguns comentarios adicionados para melhor entendimento

--- Sistema Apolices/Veiculo/frmListaDeVeiculos.cs
+++ Sistema Apolices/Veiculo/frmListaDeVeiculos.cs
@@ -19,6 +19,7 @@ namespace Sistema_Apolices
         {
             InitializeComponent();
 
+            //Organiza datagrid
             dgvCarros.ColumnCount = 6;
 
             dgvCarros.Columns[0].Name = "Código";
@@ -29,6 +30,7 @@ namespace Sistema_Apolices
             dgvCarros.Columns[5].Name = "Renavam";
             dgvCarros.Columns[3].Width = 130;
 
+            //Atualiza datagrid
             AtualizarDgv(); 
             
             //Caso não possua registros o alterar fica desabilitado
@@ -45,7 +47,11 @@ namespace Sistema_Apolices
             {
                 List<Carro> carros;
                 carros = new CarroController().Listar();
+                
+                //Limpa datagrid
                 dgvCarros.Rows.Clear();
+
+                //Popula datagrid
                 foreach (Carro item in carros)
                 {
                     dgvCarros.Rows.Add(item.id.ToString(), item.modelo.marca.nome, item.modelo.nome, item.chassi, item.placa, item.renavam);
@@ -81,12 +87,14 @@ namespace Sistema_Apolices
         {
             try
             {
+                //Preenche o objeto carro
                 Carro carro = new Carro();
                 carro.id = int.Parse(dgvCarros.SelectedRows[0].Cells[0].Value.ToString());
                 carro.chassi = dgvCarros.SelectedRows[0].Cells[3].Value.ToString();
                 carro.placa = dgvCarros.SelectedRows[0].Cells[4].Value.ToString();
                 carro.renavam = dgvCarros.SelectedRows[0].Cells[5].Value.ToString();
 
+                //Envia o objeto para próxima janela
                 frmAlterarVeiculo janela = new frmAlterarVeiculo(carro);
                 if (janela.ShowDialog() == DialogResult.OK)
                 {
